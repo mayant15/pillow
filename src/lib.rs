573f786566c6731bcf1a935db23d3587a61be598
Mayant Mukul <mayantmukul@outlook.in>
2022-04-10 12:16:09 +0530

    refactor(parser): generate AST directly

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,8 @@ mod codegen;
 mod optimizer;
 mod parser;
 
-mod tree;
-
-// NOTE: compile takes ownership of the program string.
-// The Lexer looks at views on this string only
-pub fn compile(program: &str) -> Result<(), &'static str> {
-    let mut lexer = parser::Lexer::new(&program);
-
-    let mut tokens: Vec<parser::Token> = Vec::new();
-    while let Some(token) = lexer.get_next_token() {
-        tokens.push(token)
-    }
-
-    let ast = parser::parse(tokens);
+pub fn compile(program: &str) -> Result<(), &str> {
+    let ast = parser::parse(program);
     let new_ast = optimizer::optimize(ast);
     let ir = codegen::generate(new_ast);
     println!("{:?}", ir);
--- src/parser/mod.rs
+++
@@ -1,5 +0,0 @@
-pub use self::ast::parse;
-pub use self::lexer::{Lexer, Token};
-
-mod ast;
-mod lexer;
